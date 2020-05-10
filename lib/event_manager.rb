5dba7745f9eae0406e58ea3917b451230f2a2f2e
Rafael Gomes <rafaelg1402@protonmail.com>
2020-05-10 09:45:42 +0100

    create clean_zipcode method to display method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,12 @@
 require "csv"
+
+def clean_zipcode(zipcodes)
+  zipcodes.to_s.rjust(5,"0")[0..4]
+end
+
 event_attendees_content = CSV.open "../event_attendees.csv",headers:true,header_converters: :symbol
 event_attendees_content.each do |row|
   names = row[:first_name]
-  zipcodes = row[:zipcode]
+  zipcodes = clean_zipcode(row[:zipcode])
   p "#{names} #{zipcodes}"
 end
