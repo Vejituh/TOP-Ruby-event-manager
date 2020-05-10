5cc64a1c243c051c3755b2f79dca894aa249f873
Rafael Gomes <rafaelg1402@protonmail.com>
2020-05-10 10:05:11 +0100

    add google api through gem install

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require "csv"
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcodes)
   zipcodes.to_s.rjust(5,"0")[0..4]
@@ -8,5 +12,15 @@ event_attendees_content = CSV.open "../event_attendees.csv",headers:true,header_
 event_attendees_content.each do |row|
   names = row[:first_name]
   zipcodes = clean_zipcode(row[:zipcode])
-  p "#{names} #{zipcodes}"
+  
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcodes,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+    legislators = legislators.officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+  p "#{names} #{zipcodes} #{legislators}"
 end
